0523c5d6d15782082ec275146f907669f8fe6fc9
Seunggun Lee <seunggy123@gmail.com>
2018-05-03 00:10:03 -0400

    snake moves with retro feel

---
+++ src/snake_Game/Cell.java
@@ -0,0 +1,13 @@
+package snake_Game;
+
+import processing.core.PApplet;
+
+public class Cell extends PApplet{
+	public float positionX;
+	public float positionY;
+	
+	public Cell() {
+		this.positionX = 5;
+		this.positionY = 5;
+	}
+}
